6db6bc8812d8a6862f3b2affae3ea22058494c1f
MagmarU <meneskuchno@mail.ru>
2022-08-08 16:44:53 +0300

    Accumulator

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 "use strict";
 
-function Calculator() {
+function Accumulator(startingValue) {
+    this.value = startingValue;
     this.read = function() {
-        this.firstNumber = +prompt('Введите первое число: ', '');
-        this.secondNuumber = +prompt('Введите второе число: ','');
+        return ( this.value += +prompt("Введите число: ", '') );
     };
-    this.sum = function() {
-        return this.firstNumber + this.secondNuumber;
-    };
-    this.mul = function() {
-        return this.firstNumber * this.secondNuumber;
-    };
-}
 
-let calculator = new Calculator();
-calculator.read();
-console.log( "Sum = " + calculator.sum() );
-console.log( "Mul = " + calculator.mul() );
+}
+let startingValue = 1;
+let accumulator = new Accumulator(startingValue);
+
+accumulator.read();
+
+console.log(accumulator.value);
