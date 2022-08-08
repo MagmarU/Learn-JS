489e09a3f729885265eeef935378c646a534917b
MagmarU <meneskuchno@mail.ru>
2022-08-08 13:50:39 +0300

    Ready-made_solution

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 "use strict";
 
 function Calculator() {
-    this.read: () {
+    this.read = function() {
         this.firstNumber = +prompt('Введите первое число: ', '');
         this.secondNuumber = +prompt('Введите второе число: ','');
-    },
+    };
+    this.sum = function() {
+        return this.firstNumber + this.secondNuumber;
+    };
+    this.mul = function() {
+        return this.firstNumber * this.secondNuumber;
+    };
 }
+
+let calculator = new Calculator();
+calculator.read();
+console.log( "Sum = " + calculator.sum() );
+console.log( "Mul = " + calculator.mul() );
