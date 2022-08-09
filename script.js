5ae6680dca0248238ca8293a1537e2b02def8825
MagmarU <meneskuchno@mail.ru>
2022-08-09 14:33:37 +0300

    Symbol_properties

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 "use strict";
 let id = Symbol("id");
 let user = {
-  [id] : 1
+  [id]: 1,
+  [id]: 2,
+  name: 'John',
+  name: 'Daniel',
 };
 
-for( let prop in user ) {
-  console.log( user[prop] );
-}
+console.log(Symbol.for("id"));
+console.log(user[id], user.name);
+
+// let id = Symbol("id");
+// let user = {
+//   [id]: 1,
+// };
+
+// id = null;
+// console.log(user[id]);
