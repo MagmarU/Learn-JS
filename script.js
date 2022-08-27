de5905f71bcc35a14d5d0b624ae20abb5189d7f2
MagmarU <meneskuchno@mail.ru>
2022-08-27 21:28:18 +0300

    uniqueValueArr_BadSolutionMethod

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-function getAverageAge( arr ) {
-  return arr.reduce( ( countAge, item ) => countAge + item.age, 0 ) / arr.length;
+function unique(stringArray) {
+  let result_Arr_With_Unique_String_Value = [];
+  let uniqueStr;
+  for (let item in stringArray) {
+    uniqueStr = stringArray[item];
+    if (stringArray.length == 1) {
+      result_Arr_With_Unique_String_Value.push(stringArray[0]);
+      break;
+    } else {
+      stringArray = stringArray.filter(item => item != uniqueStr);
+      result_Arr_With_Unique_String_Value.push(uniqueStr);
+    }
+  }
+  return result_Arr_With_Unique_String_Value;
 }
 
-let vasya = { name: "Вася", age: 25 };
-let petya = { name: "Петя", age: 30 };
-let masha = { name: "Маша", age: 29 };
 
-let arr = [ vasya, petya, masha ];
+let strings = ["кришна", "кришна", "харе", "харе",
+  "харе", "харе", "кришна", "кришна", ":-O"
+];
 
-console.log( getAverageAge( arr ) );
+
+for( let i = 0; i < 10000; i++ ){
+  console.log(unique(strings));
+}
