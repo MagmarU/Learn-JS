4c180e65cd6a94181d7cd374ff6ea5a1a324f174
MagmarU <meneskuchno@mail.ru>
2022-08-27 21:41:09 +0300

    filterRangeInPlace

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
-function unique(stringArray) {
-  let result_Arr_With_Unique_String_Value = [];
-  let uniqueStr;
-  for (let item in stringArray) {
-    uniqueStr = stringArray[item];
-    if (stringArray.length == 1) {
-      result_Arr_With_Unique_String_Value.push(stringArray[0]);
-      break;
-    } else {
-      stringArray = stringArray.filter(item => item != uniqueStr);
-      result_Arr_With_Unique_String_Value.push(uniqueStr);
+function filterRangeInPlace( arr, a, b ) {
+  for( let item in arr ) {
+    if( arr[ item ] < a || arr[ item ] > b ) {
+      console.log( arr[ item ] );
+      arr.splice( item, 1 );
     }
   }
-  return result_Arr_With_Unique_String_Value;
 }
+let arr = [5, 3, 8, 1];
 
-
-let strings = ["кришна", "кришна", "харе", "харе",
-  "харе", "харе", "кришна", "кришна", ":-O"
-];
-
-
-for( let i = 0; i < 10000; i++ ){
-  console.log(unique(strings));
-}
+filterRangeInPlace( arr, 1, 4 );
+console.log( arr );
