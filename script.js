e888c20c096814e3a410bbdc30520c45775d5ba3
MagmarU <meneskuchno@mail.ru>
2022-08-30 12:46:51 +0300

    patternOfSortedObject

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
-function groupById( users ) {
-    return users.reduce( ( resultObj, item ) => { 
-        resultObj[item.id] = item;
-        return resultObj;
-    } , {} );
+// let range = {
+//     from: 1,
+//     to: 5
+// };
+
+// range[Symbol.iterator] = function() {
+//     return {
+//         current: this.from,
+//         last: this.to,
+
+//         next() {
+//             if( this.current <= this.last ) {
+//                 return { done: false, value: this.current++};
+//             } else {
+//                 return { done: true };
+//             }
+//         }
+//     }
+// }
+
+let range = {
+    from: 1,
+    to: 5,
+    [Symbol.iterator]() {
+        this.current = this.from;
+        return this;
+    },
+
+    next() {
+        if (this.current <= this.to) {
+            return { done: false, value: this.current++ };
+        } else {
+            return { done: true };
+        }
+    }
+};
+
+
+for (let prop of range) {
+    console.log(prop);
 }
-
-let users = [
-    {id: 'john', name: "John Smith", age: 20},
-    {id: 'ann', name: "Ann Smith", age: 24},
-    {id: 'pete', name: "Pete Peterson", age: 31},
-  ];
-  
-  let usersById = groupById(users);
-
-  console.log( usersById );
