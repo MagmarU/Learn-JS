24e87ab59052e57f7e9b7d94021a371d8b67ff65
MagmarU <meneskuchno@mail.ru>
2022-09-01 19:05:57 +0300

    iteratedKeys

--- script.js
+++ script.js
@@ -1,39 +1,9 @@
-// function aclean( arr ) {
-//     let res = [];
-//     let resultArr = arr.map( ( element, index, array ) => {
-//         return ( ( array[index].toLowerCase().split('') ).sort() ).join('');
-//     });
+let map = new Map();
 
-//     let set = new Set( resultArr );
+map.set( "name", "John" );
 
-    
-//     for( let value of set ) {
-//         res.push( arr[ resultArr.indexOf( value ) ] );
-//     }
+let keys = Array.from( map.keys() );
 
-//     return res;
-// }
+keys.push( "more" );
 
-
-// let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
-
-// console.log( aclean( arr ) );
-
-
-
-function aclean( arr ) {
-    let resultArr = new Map();
-
-    for( let element of arr ) {
-        resultArr.set( element.toLowerCase().split('').sort().join(''), element );
-    }
-
-    return resultArr.values();
-}
-
-let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
-
-
-for( let i = 0; i < 10000; i++ ){
-    console.log( aclean( arr ) );
-}
+console.log( keys );
