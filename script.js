6d35c6385f47dceee9fe2bc33dd3b0f879db0656
MagmarU <meneskuchno@mail.ru>
2022-09-05 20:00:15 +0300

    formateDate_Prototype

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
-function getSecondsToday() {
-    let date = new Date();
-    let startDate = new Date( date );
-    startDate.setHours( 0, 0, 0 );
-    return Math.round( ( date.getTime() - startDate.setHours( 0 ) ) / 1000 );
+function formateDate( date ) {
+    let datenow = new Date();
+    let resultDate = ( datenow - date ) / 1000;
+    if( resultDate < 1 ) {
+        return 'прямо сейчас';
+    } else if( resultDate < 60 ) {
+        return `${ resultDate } сек. назад`;
+    } else if( resultDate < 3600 ) {
+        return `${ resultDate / 60 } мин. назад`;
+    } 
+    // else {
+    //     return `${  }`
+    // }
+
+
+    // switch ( resultDate ) {
+    //     case resultDate < 1:
+    //         resultDate = 'прямо сейчас';
+    //         break;
+    //     case resultDate < 60:
+    //         resultDate = `${ resultDate } сек. назад`;
+    //         break;
+    //     case resultDate < 3600:
+    //         resultDate = `${ resultDate / 60 } мин. назад`;
+    //         break;
+    //     // default:
+            
+    // }
+    // return resultDate;
 }
-console.log( getSecondsToday() );
 
-
-function getSecondsToTomorrow() {
-    let date = new Date();
-    let endDate = new Date( date.getFullYear(), date.getMonth(), date.getDate() + 1 );
-    // endDate.setDate( date.getDate() + 1 );
-    // endDate.setHours( 0, 0, 0, 0 );
-    return Math.round( ( endDate.getTime() - date.getTime() ) / 1000 ) ;
-}
-
-function getSecondsToTomorrow2() {
-    let now = new Date();
-  
-    // завтрашняя дата
-    let tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate()+1);
-  
-    let diff = tomorrow - now; // разница в миллисекундах
-    return Math.round(diff / 1000); // преобразуем в секунды
-  }
-
-  
-console.log( getSecondsToTomorrow() );
-console.log( getSecondsToTomorrow2() );
+let date = new Date(new Date - 30 * 1000);
+console.log( formateDate( date ) );
