5b48551871878340f0a9095fa571528d6d4ff526
MagmarU <meneskuchno@mail.ru>
2022-09-10 17:08:00 +0300

    rectInSquare_prototype

--- script.js
+++ script.js
@@ -1,7 +1,47 @@
-function arrayPlusArray( arr1, arr2 ) {
-  return arr1.concat( arr2 ).reduce( ( sum, item ) => item + sum );
+// function sqInRect( lng, wdth ) {
+//   let area = lng * wdth;
+//   if( lng == wdth ) return null;
+//   let minNum = Math.min( lng, wdth );
+//   let square;
+//   let result = [];
+//   while( area > 0 ) {
+//     square = Math.floor( Math.sqrt( area ) );
+//     if( square >= lng || square >= wdth ) {
+//       square = minNum;
+//     }
+//     result.push( square );
+//     area -= square ** 2;
+//     if( area == 0 ) break;
+//   }
+//   return result;
+// }
+
+// console.log( sqInRect( 20, 14 ) );
+let perimetr = ( square ) => 4 * square;
+
+function sqInRect( lng, wdth ) {
+  let perimetrMain = 2 * ( lng + wdth );
+  let square;
+  let result = [];
+  while( lng > 0 && wdth > 0 ) {
+    console.log( lng, wdth );
+    
+
+    square = Math.min( lng, wdth );
+    
+
+    result.push( square );
+    perimetrMain -= perimetr( square );
+    if( lng == 0 ) {
+      lng = ( perimetrMain / 2 ) - wdth;
+    } else {
+      wdth = ( perimetrMain / 2 ) - lng;
+    }
+    // let [ lng, wdth ] = [ lng - square, wdth - square ];
+    console.log( lng, wdth );
+  }
+  return result;
+
 }
 
-let arr1 = [1, 2, 3];
-let arr2 = [4, 5, 6];
-console.log( arrayPlusArray( arr1, arr2 ) );
+console.log( sqInRect( 20, 14 ) );
