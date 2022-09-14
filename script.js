94a72f9964c56d4e23b43f496808dc1ff9cbc38c
MagmarU <meneskuchno@mail.ru>
2022-09-14 19:42:51 +0300

    first

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-let table = document.createElement( 'table' );
-const body = document.body;
-
-for( let i = 1; i <= 5; i++ ) {
-  let currTr = document.createElement( 'tr' );
-  table.appendChild( currTr );
-  for( let j = 1; j <= 5; j++ ) {
-    let currTd = document.createElement( 'td' );
-    currTr.appendChild( currTd );
-    currTd.innerHTML = `${j}:${i}`;
-    currTr.appendChild( currTd );
-  }
-}
-
-for( let i = 0; i < 5; i++ ) {
-  let tablerow = table.rows[i].cells[i];
-  tablerow.style.background = 'red';
-}
-body.appendChild( table );
+let body = document.body;
+// body.innerHTML = '<!--ggg-->';
+Element.prototype.nodeNme = () => 'hi';
+console.log( body.querySelector('div').nodeNme() );
