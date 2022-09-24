d5c8f69cafa665500f87f607884cbb0fad9065f0
MagmarU <meneskuchno@mail.ru>
2022-09-24 15:44:41 +0300

    countNesting

--- script.js
+++ script.js
@@ -1,47 +1,46 @@
-// let sumTo = ( n ) => n == 1 ? n : n + sumTo( n - 1 );
-// console.log( sumTo( 100 ) );
+let i = 0;
+function countNesting( obj ) {
+  
+  for( let prop in obj ) {
+    let res = 0;
+    // console.log( prop );
+    if( Object.keys( obj[prop] ).length != 0 ) {
+      // console.log( obj[prop] );
+      // console.log( Object.keys( obj[prop] ).length );
+      res = Object.keys( obj[prop] ).length + countNesting( obj[prop] );
+      i += res;
+      // console.log( i );
+      console.log( prop, i );
+      
+    } else {
+      res = 0;
+    }
 
+    i = 0;  
+    return res;  
+  }
 
-// let factorial = ( n ) => n == 1 ? n : n * factorial( n - 1 );
-// console.log( factorial( 77 ) );
-
-// function fib( n ) {
-//   // let result;
-//   if( n == 1 ) {
-//     return n;
-//   } else {
-//     console.log( sum( 1, 1, n ) );
-//     n--;
-//   }
-//   // return result;
-
-
-//   // let a = 1;
-//   // if ( n == 1 ) {
-//   //   return n;
-//   // } else {
-//   //   n += a;
-//   //   a = n;
-//   // }
-//   // n--;
-// }
-
-// function sum( a, b, n ) {
-//   while( n != 1 ) {
-//   let result = a + b;
-//   a = b;
-//   b = result;
-//   arr = [ a, b ];
-//   n--;
-//   return result;
-//   // break;
-// }
-// }
-// // console.log( sum( 1, 1 ) );
-// console.log( fib( 7 ) );
-
-function fib( n ) {
-  return n <= 1 ? n : fib( n - 1 ) + fib( n - 2 );
+  // console.log( i );
+  
 }
 
-console.log( fib( 4 ) );
+let data = {
+  "Рыбы": {
+    "форель": {},
+    "лосось": {}
+  },
+
+  "Деревья": {
+    "Огромные": {
+      "секвойя": {},
+      "дуб": {},
+      "вишня": {}
+    },
+    "Цветковые": {
+      "яблоня": {},
+      "магнолия": {},
+    }
+  }
+};
+
+console.log( countNesting( data ) );
