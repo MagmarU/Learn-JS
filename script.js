cf65e5b8cceeac07e03c512b486d42fd8307d5f8
MagmarU <meneskuchno@mail.ru>
2022-10-02 18:31:30 +0300

    scrollBottom

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-function showNotification( {top, right, html, className} ) {
-  let div = document.createElement( 'div' );
-  div.className = 'notification';
-  if( className ) { div.classList.add( className ) };
-
-  div.style.top = `${top}px`;
-  div.style.right = `${right}px`;
-
-  div.innerHTML = html;
-  document.body.append( div );
-}
-
-showNotification({
-  top: 10, // 10px от верхней границы окна (по умолчанию 0px)
-  right: 10, // 10px от правого края окна (по умолчанию 0px)
-  html: "Hello!", // HTML-уведомление
-  className: "welcome" // дополнительный класс для div (необязательно)
-});
+let elem = document.body.querySelector( '.text' );
+let scrollBottom = () => elem.scrollHeight - (elem.scrollTop + elem.clientHeight);
+setInterval( () => console.log( scrollBottom() ), 100 );
