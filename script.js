20734be3e2b9a8149b0c0256d86f9874428f1071
MagmarU <meneskuchno@mail.ru>
2022-10-06 18:47:48 +0300

    noteNearbyElement

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 function positionAT( anchor, position, elem ) {
+    let positions = {
+        "top": function () {
+            elem.style.left = coords.left + 'px';
+            elem.style.top = coords.top - elem.clientHeight + 'px';
+        },
+        "right": function () {
+            elem.style.left = coords.right + 'px';
+            elem.style.top = coords.top + 'px';
+        },
+        "bottom": function () {
+            elem.style.left = coords.left + 'px';
+            elem.style.top = coords.bottom + 'px';
+        }
+    }
     
+    elem.style.cssText = 'position: absolute;';
+    let coords = anchor.getBoundingClientRect();
+    positions[position]();
 }
+
+let elem = document.querySelector('.message');
+let anchor = document.querySelector('blockquote');
+
+positionAT(anchor, "right", elem);
