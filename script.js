280a55db9d0e251d046297a83e5f45f04e9244a1
MagmarU <meneskuchno@mail.ru>
2022-10-06 18:10:06 +0300

    aa

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-let field = document.querySelector( '#field' );
-console.log( field );
-let objProp = field.getBoundingClientRect();
-let oneAngle = [objProp.x, objProp.y];
-let twoAngle = [ oneAngle[0] + objProp.width, oneAngle[1] + objProp.height ];
-let threeAngle = [ objProp.x + field.style, objProp.y + +field.style.borderTop ];
-console.log( objProp );
-console.log( field.clientWidth );
-console.log( threeAngle );
+function positionAT( anchor, position, elem ) {
+    
+}
