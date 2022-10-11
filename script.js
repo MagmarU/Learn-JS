69b6ad22c98d68529b36ab3fbdfd53f9be16600e
MagmarU <meneskuchno@mail.ru>
2022-10-11 10:34:50 +0300

    hideYourSelf_button

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
 let button = document.querySelector('#hider');
-let div = document.querySelector('#text');
 
-button.onclick = function() {
-    div.hidden = div.hidden ? false : true;
-}
+button.onclick = () => button.hidden = true;
