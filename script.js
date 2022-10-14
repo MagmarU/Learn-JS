2334ab3a2a3cecc6a0988f978b39a28ba8eadd22
MagmarU <meneskuchno@mail.ru>
2022-10-14 21:27:51 +0300

    ballMoving

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-let button = document.querySelector('#hider');
+let field = document.querySelector('#field');
+let ball = document.querySelector('#ball');
+let fieldCoords = field.getBoundingClientRect();
 
-button.onclick = () => button.hidden = true;
+field.onmouseup = function(event) {
+    let ballCoords = {
+        top: event.clientY - fieldCoords.top - ball.clientHeight / 2 - field.clientLeft,
+        left: event.clientX - fieldCoords.left - ball.clientWidth / 2 - field.clientLeft
+    };
+
+    ballCoords.top = ballCoords.top > 0 ? ballCoords.top : 0;
+
+    ballCoords.left = ballCoords.left > 0 ? ballCoords.left : 0;
+
+    if( ballCoords.top + ball.clientHeight > field.clientHeight ) {
+        ballCoords.top = field.clientHeight - ball.clientHeight;
+    }
+
+    if( ballCoords.left + ball.clientWidth > field.clientWidth ) {
+        ballCoords.left = field.clientWidth - ball.clientWidth;
+    }
+    ball.style.top = ballCoords.top + 'px';
+    ball.style.left = ballCoords.left + 'px';
+}
