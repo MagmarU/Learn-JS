a3d3c08ec3235f162c5d05b8a2568f81f4bac975
MagmarU <meneskuchno@mail.ru>
2022-10-15 14:59:53 +0300

    delete_Element

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-let list = document.querySelector('span');
-let ul = document.querySelector('ul');
-let text = list.innerHTML.split(' ').slice(1).join(' ');
+let pane = document.querySelectorAll('.pane');
+let button = '<button class="remove-button">[x]</button>';
 
-function trigger( hiddenStatus ) {
-    let char = {
-        false: '▶ ',
-        true: '▼ '
-    };
-    list.innerHTML = char[hiddenStatus] + text;
-    return !hiddenStatus;
+for( let elem of pane ) {
+    elem.insertAdjacentHTML( 'afterbegin', '<button class="remove-button">[x]</button>' );
+    elem.firstChild.onclick = () => elem.remove();
 }
-
-list.onclick = function() {
-    ul.hidden = ul.hidden ? trigger(true) : trigger(false);
-};
