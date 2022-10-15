8e46a57a546f948d27ea1e233d35adedcb7988a3
MagmarU <meneskuchno@mail.ru>
2022-10-15 13:20:48 +0300

    showHidden_List

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-let field = document.querySelector('#field');
-let ball = document.querySelector('#ball');
-let fieldCoords = field.getBoundingClientRect();
+let list = document.querySelector('span');
+let ul = document.querySelector('ul');
+let text = list.innerHTML.split(' ').slice(1).join(' ');
 
-field.onmouseup = function(event) {
-    let ballCoords = {
-        top: event.clientY - fieldCoords.top - ball.clientHeight / 2 - field.clientLeft,
-        left: event.clientX - fieldCoords.left - ball.clientWidth / 2 - field.clientLeft
+function trigger( hiddenStatus ) {
+    let char = {
+        false: '▶ ',
+        true: '▼ '
     };
-
-    ballCoords.top = ballCoords.top > 0 ? ballCoords.top : 0;
-
-    ballCoords.left = ballCoords.left > 0 ? ballCoords.left : 0;
-
-    if( ballCoords.top + ball.clientHeight > field.clientHeight ) {
-        ballCoords.top = field.clientHeight - ball.clientHeight;
-    }
-
-    if( ballCoords.left + ball.clientWidth > field.clientWidth ) {
-        ballCoords.left = field.clientWidth - ball.clientWidth;
-    }
-    ball.style.top = ballCoords.top + 'px';
-    ball.style.left = ballCoords.left + 'px';
+    list.innerHTML = char[hiddenStatus] + text;
+    return !hiddenStatus;
 }
+
+list.onclick = function() {
+    ul.hidden = ul.hidden ? trigger(true) : trigger(false);
+};
