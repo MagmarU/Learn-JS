863de9a8f34c15f6844efbfd4d587daf5316d63d
MagmarU <meneskuchno@mail.ru>
2022-10-18 19:05:42 +0300

    delegationForDeleteElement

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-let div = document.querySelector('.gallery');
-let pics = document.querySelector('.pics');
-console.log(pics)
-const leftButt = div.firstElementChild;
-const rightBut = div.lastElementChild;
-leftButt.onclick = function() {
-    pics.scrollLeft -= 390;
-}
-rightBut.onclick = function() {
-    pics.scrollLeft += 390;
-}
+let container = document.querySelector('#container');
+
+container.addEventListener( 'click', function(event) {
+    
+    if( event.target.className == 'remove-button' ) {
+        event.target.closest('.pane').remove();
+    }
+});
