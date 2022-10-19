b56e94449437964583769e787ee320153a704fa8
MagmarU <meneskuchno@mail.ru>
2022-10-19 22:11:35 +0300

    dropdown_Tree

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-let container = document.querySelector('#container');
-
-container.addEventListener( 'click', function(event) {
-    
-    if( event.target.className == 'remove-button' ) {
-        event.target.closest('.pane').remove();
-    }
+let tree = document.querySelector( '.tree' );
+tree.addEventListener( 'click', function(event) {
+    if(event.target.tagName != 'SPAN') return;
+    let child = event.target.nextElementSibling;
+    child.hidden = !child.hidden;
 });
