1bd46fe74104149598769c93f1b0a54e9cdcd261
MagmarU <meneskuchno@mail.ru>
2022-10-22 14:54:52 +0300

    sortable_TablebyColumn

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 let table = document.querySelector( 'table' );
 
-let tr = Array.from( table.rows )
-  .slice(1)
-  .sort( (a, b) => a.cells[0].innerHTML < b.cells[0].innerHTML ? -1 : 1 );
+let handler = {
+    number(tr) {
+        return tr.sort( ( a, b ) => +a.cells[0].innerHTML - +b.cells[0].innerHTML );
+    }, 
+    string(tr){
+        return tr.sort( ( a, b ) => a.cells[1].innerHTML < b.cells[1].innerHTML ? -1 : 1 );
+    }
+}
 
+table.addEventListener( 'click', function(event) {
+    let tr = Array.from( table.rows ).slice(1);
+    let result = handler[event.target.dataset.type](tr);
 
-table.tBodies[0].append( ...tr );
+    table.tBodies[0].append( ...result );
+});
