ded706642a9947725b74a18fc59d8395c6ffb6dd
MagmarU <meneskuchno@mail.ru>
2022-10-23 15:08:31 +0300

    workingCode

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
-function getCoords(anchor) {
-    let elem = anchor.getBoundingClientRect();
-    return {
-        top: elem.top + window.pageYOffset,
-        right: elem.right + window.pageXOffset,
-        left: elem.left + window.pageXOffset,
-        bottom: elem.bottom + window.pageYOffset
-    };
-
+function positionDiv(div, place, coords, target) {
+    let position = {
+        top() {
+            div.style.top = coords.top - div.offsetHeight + window.pageYOffset - 5 + 'px';
+            this.left();
+        },
+        down() {
+            div.style.top = coords.bottom + window.pageYOffset + 5 + 'px';
+            this.left();
+        },
+        left() {
+            let left = coords.left + target.clientWidth / 2 - div.clientWidth/2;
+            div.style.left = left < 0 ? '0px' : `${left}px`;
+        }
+    }
+    position[place]();
 }
 
 document.addEventListener('mouseover', function (event) {
     if (!event.target.dataset.tooltip) return;
-    let div = document.createElement('span');
-    
-    div.style.cssText = 'display: absolute';
-    document.body.append( div );
-    let coords = getCoords( event.target );
-    console.log( coords );
+    let div = document.body.querySelector('.tooltip');
+
+    div.style.cssText = 'position: absolute';
+    document.body.append(div);
+    let coords = event.target.getBoundingClientRect();
+
     div.innerHTML = event.target.dataset.tooltip;
 
-    // div.style.left = coords.left + 'px';
-    // div.style.top = coords.top - div.offsetHeight + 'px';
-    
+    if (coords.top - 5 - div.offsetHeight < 0) {
+        positionDiv(div, 'down', coords, event.target)
+    } else {
+        positionDiv(div, 'top', coords, event.target)
+    };
+
+});
+
+document.addEventListener('mouseout', function () {
+    let div = document.querySelector('.tooltip');
+    div.style.cssText = 'display: none;';
 });
