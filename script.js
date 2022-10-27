b2f6f69b346d06c7412dee9b4e51f273bca3fd76
MagmarU <meneskuchno@mail.ru>
2022-10-27 15:23:21 +0300

    goOver_byLink

--- script.js
+++ script.js
@@ -1,46 +1,7 @@
-let i = 0;
-function countNesting( obj ) {
-  
-  for( let prop in obj ) {
-    let res = 0;
-    // console.log( prop );
-    if( Object.keys( obj[prop] ).length != 0 ) {
-      // console.log( obj[prop] );
-      // console.log( Object.keys( obj[prop] ).length );
-      res = Object.keys( obj[prop] ).length + countNesting( obj[prop] );
-      i += res;
-      // console.log( i );
-      console.log( prop, i );
-      
-    } else {
-      res = 0;
-    }
-
-    i = 0;  
-    return res;  
-  }
-
-  // console.log( i );
-  
-}
-
-let data = {
-  "Рыбы": {
-    "форель": {},
-    "лосось": {}
-  },
-
-  "Деревья": {
-    "Огромные": {
-      "секвойя": {},
-      "дуб": {},
-      "вишня": {}
-    },
-    "Цветковые": {
-      "яблоня": {},
-      "магнолия": {},
-    }
-  }
-};
-
-console.log( countNesting( data ) );
+document.addEventListener( 'click', function( event ) {
+  let eventName = event.target;
+  if( eventName.closest('a') ) {
+    if(confirm(`Перейти на страницу ${eventName.getAttribute('href')} ?`) ) return;
+    event.preventDefault();
+  } 
+});
