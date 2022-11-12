aff41b940b942d90b00a333082c6e2fe6ad412f1
MagmarU <meneskuchno@mail.ru>
2022-11-12 21:44:46 +0300

    v0.0.2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // function positionDiv(div, place, coords, target) { 
-    
+
 //     let position = {
 //         top() {
 //             div.style.top = coords.top - div.offsetHeight + window.pageYOffset - 5 + 'px';
@@ -29,7 +29,7 @@
 // });
 
 // function handler( event ) {
-    
+
 //     let div = document.querySelector( '.tooltip' );
 //     div.style.cssText = 'position: absolute';
 //     div.innerHTML = event.dataset.tooltip;
@@ -53,21 +53,21 @@
 
 
 
-function positionDiv(div, place, coords, target) { 
-    
+function positionDiv(div, place, coords, target) {
+
   let position = {
-      top() {
-          div.style.top = coords.top - div.offsetHeight + window.pageYOffset - 5 + 'px';
-          this.left();
-      },
-      down() {
-          div.style.top = coords.bottom + window.pageYOffset + 5 + 'px';
-          this.left();
-      },
-      left() {
-          let left = coords.left + target.offsetWidth / 2 - div.offsetWidth / 2;
-          div.style.left = left < 0 ? '0px' : `${left}px`;
-      }
+    top() {
+      div.style.top = coords.top - div.offsetHeight + window.pageYOffset - 5 + 'px';
+      this.left();
+    },
+    down() {
+      div.style.top = coords.bottom + window.pageYOffset + 5 + 'px';
+      this.left();
+    },
+    left() {
+      let left = coords.left + target.offsetWidth / 2 - div.offsetWidth / 2;
+      div.style.left = left < 0 ? '0px' : `${left}px`;
+    }
   }
   position[place]();
 }
@@ -76,14 +76,30 @@ function positionDiv(div, place, coords, target) {
 
 
 
+function calculationSpeed(options) {
+  let prevEvent, currentEvent, speed;
 
+  options.elem.onmousemove = function (event) {
+    currentEvent = event;
+  }
 
+  setInterval(function () {
+    if (prevEvent && currentEvent) {
+      let movementX = Math.abs(currentEvent.screenX - prevEvent.screenX);
+      let movementY = Math.abs(currentEvent.screenY - prevEvent.screenY);
+      let movement = Math.sqrt(movementX * movementX + movementY * movementY);
 
+      speed = Math.round( 10 * movement );
+      // console.log( prevEvent );
+      if (speed < 50) {
+        options.over();
+      }
+      // document.getElementById("speed").innerText = Math.round(speed);
+    }
 
-
-
-
-
+    prevEvent = currentEvent;
+  }, 1);
+}
 
 
 
@@ -91,18 +107,26 @@ function positionDiv(div, place, coords, target) {
 let tooltip = document.querySelector('#tooltip');
 
 
-function HoverIntent( options ) {
-  options.elem.addEventListener( 'mouseenter', function() {
-    // console.log(  );
-    options.over();
-  });
-  options.elem.addEventListener( 'mouseleave', function() {
-    options.out();
-  });
+function HoverIntent(options) {
+
+  options.elem.addEventListener( 'mouseenter', (event) => calculationSpeed(options) );
+  // options.elem.addEventListener( 'mouseleave', () => options.out() );
+  // options.elem.addEventListener( 'mouseenter', function(event) {
+  //   console.log( event.clientX, event.clientY );
+  //   // console.log(  );
+  //   options.over();
+  // });
+  // options.elem.addEventListener( 'mouseleave', function() {
+  //   options.out();
+  // });
+
+
+
+
   // console.log( elem );
 
-  
-  
+
+
   // this.elem.addEventListener( 'mousemove', function(event) {
   //   console.group();
   //   console.log( event.pageX , event.pageY );
@@ -110,32 +134,29 @@ function HoverIntent( options ) {
   // });
 }
 
-let clock = document.querySelector( '.clock' );
+let clock = document.querySelector('.clock');
 // clock.addEventListener( 'mousemove', function( event ) {
 //   console.log( event.target );
 
 // });
 
 // console.log( tooltip );
-setTimeout(function() {
-    new HoverIntent({
-      elem: clock,
-      over() {
-        tooltip.hidden = false;
-        console.log( this.elem.getBoundingClientRect() );
-        positionDiv( tooltip, 'down', this.elem.getBoundingClientRect(), this.elem )
+setTimeout(function () {
+  new HoverIntent({
+    elem: clock,
+    over() {
+      tooltip.hidden = false;
+      positionDiv(tooltip, 'down', this.elem.getBoundingClientRect(), this.elem)
 
-        // let coords = this.elem.getBoundingClientRect();
-        // tooltip.style.top = coords.bottom + window.pageYOffset + 5 + 'px';
-        
-        // let left = coords.left - tooltip.offsetWidth / 2;
-        // tooltip.style.left = left < 0 ? '0px' : `${left}px`;
+      // let coords = this.elem.getBoundingClientRect();
+      // tooltip.style.top = coords.bottom + window.pageYOffset + 5 + 'px';
 
-        console.log( tooltip.hidden );
-      },
-      out() {
-        tooltip.hidden = true;
-        console.log( tooltip.hidden );
-      }
-    });
-  }, 0);
+      // let left = coords.left - tooltip.offsetWidth / 2;
+      // tooltip.style.left = left < 0 ? '0px' : `${left}px`;
+
+    },
+    out() {
+      tooltip.hidden = true;
+    }
+  });
+}, 0);
