309e678cadbddbaf66992504b9704c664d868c2e
MagmarU <meneskuchno@mail.ru>
2022-12-15 22:19:56 +0300

    arrayFilter

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-function sum(n) {
-    return function(m) {
-        console.log( n + m );
+let arr = [1,2,3,4,5,6,7,8,9];
+
+function inBetween(a, b) {
+    return function(num) {
+        return num >= a && num <= b;
+    };
+};
+
+function inArray(arr) {
+    return function(num) {
+        return arr.includes( num );
     }
 }
 
-sum(5)(2);
+console.log(arr.filter(inBetween(5,7)) );
+console.log( arr.filter(inArray([1,10,8])) );
