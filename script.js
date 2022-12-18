92b3409ad3928b0f38e458e2851f473328b1ac2a
MagmarU <meneskuchno@mail.ru>
2022-12-18 11:14:28 +0300

    shooters

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-let users = [
-    { name: "John", age: 20, surname: "Johnson" },
-    { name: "Pete", age: 18, surname: "Peterson" },
-    { name: "Ann", age: 19, surname: "Hathaway" }
-];
+function makeArmy() {
+    let shooters = [];
+  
+    for( let i = 0; i < 10; i++ ) {
 
-function byField(fieldName) {
-    return ( a, b ) => a[fieldName] > b[fieldName] ? 1 : -1;
-}
-
-users.sort( byField('surname') );
-console.log( users );
+        console.log( i );
+      let shooter = function() {
+        alert( i );
+      };
+      shooters.push(shooter);
+    }
+    console.log( shooters );
+    return shooters;
+  }
+  
+  let army = makeArmy();
+  
+  army[0]();
+  army[5]();
