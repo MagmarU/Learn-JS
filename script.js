a3d51a02b3bced40f6c6376f4b41f12562b0133e
MagmarU <meneskuchno@mail.ru>
2022-12-24 12:36:08 +0300

    setInterval

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-function sum(n) {
-  console.log( sum.length );
-}
+// function printNumbers( from, to ) {
+//   setInterval( function print() {
+//     if( from <= to ) {
+//       console.log( from );
+//       from++;
+//     }
+//   }, 1000 );
+// }
 
-sum(5)(6);
+function printNumbers( from, to ) {
+  let timer = setTimeout( function print() {
+    console.log( from );
+    if( from < to ) {
+      
+      timer = setTimeout( print, 1000 );
+    }
+    from++;
+  }, 1000 );
+}
+printNumbers( 1, 6 );
