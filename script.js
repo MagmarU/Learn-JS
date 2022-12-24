766d1a63293a9493d18264332a22a6296f0709d7
MagmarU <meneskuchno@mail.ru>
2022-12-24 12:20:14 +0300

    0

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-function bouncingBall(h,  bounce,  window) {
-  if( h <= 0 || bounce <= 0 || bounce >= 1 || window < h ) return -1;
-  let sum = 0;
-  while( h >= 0 ) {
-    sum++
-    h = h * bounce;
-    if( h > window ) sum++;
-    else break;
-  }
-  return sum;
-  // console.log( sum );
+function sum(n) {
+  console.log( sum.length );
 }
 
-console.log( bouncingBall( 10, 0.6, 10 ) );
+sum(5)(6);
